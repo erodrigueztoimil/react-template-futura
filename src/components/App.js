1070bb77d46b4cf32fb078b0e138eaf10ba0862b
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2019-06-26 22:53:47 -0400

    project started

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h3>app</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
