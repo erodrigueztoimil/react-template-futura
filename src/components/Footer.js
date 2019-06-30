85e7973cab3cbeac4b1630d734af7f34ebe01e08
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2019-06-29 22:00:36 -0400

    finished footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/stylesheets/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h3>&copy; 2019 Futura Logistics</h3>
+    </div>
+  );
+}
+
+export { Footer };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Home.js';
 export * from './About.js';
 export * from './Support.js';
+export * from './Footer.js';
